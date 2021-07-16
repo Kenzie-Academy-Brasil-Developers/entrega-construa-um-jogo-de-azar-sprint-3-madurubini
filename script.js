a5438e5f643995067625a4ba6df93af4a7c67c6b
Madu Rubini <mariaed.rubini@gmail.com>
2021-07-16 15:02:29 -0300

    Botão que exibe lógica na tela e botão de reset incluídos

--- script.js
+++ script.js
@@ -2,36 +2,79 @@
 const sorteador = ()  => Math.floor(Math.random() * 5 + 1 );
 
 /*Função que simula uma rodada*/
+
 function oneTurn(){
     let resultado = [];
     for(let i = 0; i < 3; i++){
         resultado += (sorteador())
     }
-       return resultado
+   
+     return resultado
 }
 
 /*Função que compara o resultado*/
+let valor = "";
 function analyser(resultado){
     let condicoesDeVitoria = ['111','222','333','444','555']
     let vitoria = false;
+    
 
     for(let condicao = 0; condicao< condicoesDeVitoria.length; condicao++){
         if(condicoesDeVitoria[condicao] === resultado ){
             vitoria = true;
-            console.log('Você GANHOUUUUU ' + resultado)
+            console.log(resultado) 
+            const mostrarResultado = document.getElementById("resultado");
+            let texto = document.createElement("p")
+            texto.textContent = ('Você ganhou ')
+            mostrarResultado.appendChild(texto)
+              
         }               
     } 
 
     if(!vitoria){
-        console.log('Você perdeu ' +resultado)
+        console.log(resultado)
+        const mostrarResultado = document.getElementById("resultado");
+            let texto = document.createElement("p")
+            texto.classList.add("resposta")
+            texto.textContent = ('Você perdeu ')
+            mostrarResultado.appendChild(texto) 
+           
+           
     }
     
-    return resultado
+    valor = resultado
+    return valor
 }
 
-analyser(oneTurn());
+
+
+ /*DOM*/
+
+ const result = () =>{
+    const mostrarResultado = document.getElementById("resultado");
+    let texto = document.createElement("p")
+    texto.classList.add("resposta")
+    texto.textContent = (valor)
+    mostrarResultado.appendChild(texto)  
+
+}
+
+const esconder = () => {
+    let esconderResultado = document.getElementById("resultado")
+    esconderResultado.textContent = ("");
+}
+
 
 const buttonJogar = document.getElementById('jogar');
 buttonJogar.addEventListener('click', function(){
     analyser(oneTurn());
+    result();    
 })
+
+const buttonReset = document.getElementById('reset');
+buttonReset.addEventListener('click', function(){
+    
+    esconder();
+})
+
+
